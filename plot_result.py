cdcc7f4d0f246e636876447bb25e43b6ae43efb8
kHan0809 <kimhangyeol14@gmail.com>
2022-11-19 19:59:11 +0900

    .
    .

--- plot_result.py
+++ plot_result.py
@@ -7,34 +7,45 @@ import d4rl
 
 import utils
 import TD3_BC
-
+import matplotlib.pyplot as plt
 if __name__ == "__main__":
 
 	parser = argparse.ArgumentParser()
 	# Experiment
 	parser.add_argument("--policy", default="TD3_BC")               # Policy name
-	parser.add_argument("--env", default="hopper-medium-replay-v0")        # OpenAI gym environment name
 	parser.add_argument("--seed", default=0, type=int)              # Sets Gym, PyTorch and Numpy seeds
-	parser.add_argument("--eval_freq", default=5e3, type=int)       # How often (time steps) we evaluate
-	parser.add_argument("--max_timesteps",   default=1e6, type=int)   # Max time steps to run environment
-	parser.add_argument("--max_timesteps_Q", default=3e4, type=int)  # Max time steps to run environment
-	parser.add_argument("--save_model", action="store_true")        # Save model and optimizer parameters
-	parser.add_argument("--load_model", default="")                 # Model load file name, "" doesn't load, "default" uses file_name
-	# TD3
-	parser.add_argument("--expl_noise", default=0.1)                # Std of Gaussian exploration noise
-	parser.add_argument("--batch_size", default=256, type=int)      # Batch size for both actor and critic
-	parser.add_argument("--discount", default=0.99)                 # Discount factor
-	parser.add_argument("--tau", default=0.005)                     # Target network update rate
-	parser.add_argument("--policy_noise", default=0.2)              # Noise added to target policy during critic update
-	parser.add_argument("--noise_clip", default=0.5)                # Range to clip target policy noise
-	parser.add_argument("--policy_freq", default=2, type=int)       # Frequency of delayed policy updates
-	# TD3 + BC
-	parser.add_argument("--alpha", default=2.5)
-	parser.add_argument("--normalize", default=True)
 	args = parser.parse_args()
 
-	file_name = f"{args.policy}_{args.env}_{args.seed}"
+	envs = [
+		"halfcheetah-random-v0",
+		"hopper-random-v0",
+		"walker2d-random-v0",
+		"halfcheetah-medium-v0",
+		"hopper-medium-v0",
+		"walker2d-medium-v0",
+		"halfcheetah-expert-v0",
+		"hopper-expert-v0",
+		"walker2d-expert-v0",
+		"halfcheetah-medium-expert-v0",
+		"hopper-medium-expert-v0",
+		"walker2d-medium-expert-v0",
+		"halfcheetah-medium-replay-v0",
+		"hopper-medium-replay-v0",
+		"walker2d-medium-replay-v0",
+	]
 	p_dir = "./results/"
-	ext   = ".npy"
+	ext = ".npy"
 
-	print(np.load(p_dir+file_name+ext))
+	for idx,env in enumerate(envs):
+		file_name = f"{args.policy}_{env}_{args.seed}"
+		data = np.load(p_dir + file_name + ext)
+		plt.subplot(3,5,idx+1)
+		plt.plot(data)
+		plt.title(env)
+		plt.xlabel("time steps")
+	plt.show()
+
+
+
+
+
